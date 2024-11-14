12d5da77c97fadecdbfb8aa10c2f3033346c3919
Irez Abdullah <irezabdullah@rezweb.my.id>
2024-11-15 04:07:17 +0700

    refactor: menghapus sidebar di beberapa halaman. - Menghapus sidebar pada UserManagement, ComplainList, dan CreateReport

--- src/pages/admin/ComplainList.tsx
+++ src/pages/admin/ComplainList.tsx
@@ -1,11 +1,9 @@
-import SideBar from "../../components/elements/Sidebar/sidebar";
 import Complain from "../../components/elements/table/admin/contentComplainList";
 
 const ComplaintList = () => {
     return (
         <>
             <div className="flex min-h-screen bg-gray-100">
-                <SideBar />
                 <Complain />
             </div>
         </>
